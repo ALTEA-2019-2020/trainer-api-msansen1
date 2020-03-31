370340413d46c27794b677e0b45e045ad12e0186
mathieu <msansen@gmail.com>
2020-03-31 23:46:04 +0200

    fix authorise swagger urls without auth

--- src/main/java/com/miage/altea/trainer_api/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/trainer_api/config/SecurityConfig.java
@@ -7,9 +7,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        //super.configure(http);
         http.csrf().disable();
+        http.authorizeRequests().
+                antMatchers(AUTH_WHITELIST).permitAll().  // whitelist Swagger UI resources
+                antMatchers("/**").authenticated();  // require authentication for any endpoint that's not whitelisted
     }
 }
